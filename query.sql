8e0cc3e386d31040b8af25e018a4a1083c6c4e52
zeentan22 <e0725293@u.nus.edu>
2023-01-01 16:46:12 +0800

    added final dashboard, sql script for creating database and relevant csv files for tableau

--- query.sql
+++ query.sql
@@ -17,3 +17,19 @@ FROM payments
 GROUP BY (YEAR(payments.paymentDate) * 100) + MONTH(payments.paymentDate)
 ORDER BY yyyyMM;
 
+-- find country origin of customers
+SELECT 
+    country, COUNT(country) AS count_countries
+FROM
+    customers 
+GROUP BY country
+ORDER BY count_countries DESC
+LIMIT 10;
+
+
+-- top 10 best performing sales rep
+SELECT CONCAT(e.lastName, " ", e.firstName) as name, COUNT(c.salesRepEmployeeNumber) as customers_served
+FROM employees e
+INNER JOIN customers c ON c.salesRepEmployeeNumber = e.employeeNumber
+GROUP BY c.salesRepEmployeeNumber
+ORDER BY customers_served DESC;
